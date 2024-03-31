79175dcb763e0234bded87b2de740653d97a2f34
parikshith078 <palegar.parikshith@gmail.com>
2024-03-31 07:58:46 +0530

    shadcn added as dev dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Button> Click me</Button>
+    </div>
+  );
 }
