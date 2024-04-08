c81f681fb0dad7a87f03f4200c4d52c08f6fedc6
parikshith078 <palegar.parikshith@gmail.com>
2024-04-08 07:39:20 +0530

    Added globar navbar
    squash me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from "@/components/ui/button";
 import Nav from "@/components/ui/nav";
-import { adminAuth } from "@/db/pocketbase";
 
 export default async function Home() {
-  const data = await adminAuth()
-
-  console.log(data)
 
   return (
     <div>
-      <Nav/>
     </div>
   );
 }
